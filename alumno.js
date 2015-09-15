80a1e1ba17b6a22d350c5aa85e7d36d2b2815e89
francoramiro <fedelc.us@gmail.com>
2015-09-15 20:36:45 -0300

    Promisfy Alumno

--- alumno.js
+++ alumno.js
@@ -35,7 +35,7 @@ function registrarAlumno(cont) {
   var alumno = { nombre: 'alumno piola'};
   var url = "http://" + APP_HOST + "/alumnos";
 
-  Helper.makePostPromise(alumno, url).then(function {
+  Helper.makePostPromise(alumno, url).then(function() {
     console.log("Response status de registrar alumno: " + response.statusCode + " and body: " + JSON.stringify(body));
     cont(body.id);
   })
